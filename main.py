dd3603414dc0214139f385ca5fd0e5291e98b452
Angélica Martiniano <angiihsilva@gmail.com>
2025-03-27 11:27:25 -0300

    add rotation

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # throughout this file
 import pygame
 from constants import * 
+from player import Player
 
 def main():
     print("Starting Asteroids!")
@@ -12,14 +13,18 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     
 
     while True:
       for event in pygame.event.get():
         if event.type == pygame.QUIT:
           return
-        
+      
+      player.update(dt)
+      
       screen.fill(0x0000)
+      player.draw(screen)
       pygame.display.flip()
       # limit the framerate to 60 FPS
       dt = clock.tick(60) / 1000
