906c9d13a64d38ee2fcfdcd8bc2f3b0281dd9dea
Angélica Martiniano <angiihsilva@gmail.com>
2025-03-27 11:14:17 -0300

    add fps tick

--- main.py
+++ main.py
@@ -5,11 +5,14 @@ import pygame
 from constants import * 
 
 def main():
-    pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    
 
     while True:
       for event in pygame.event.get():
@@ -18,6 +21,8 @@ def main():
         
       screen.fill(0x0000)
       pygame.display.flip()
+      # limit the framerate to 60 FPS
+      dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
